49a1f0bca0f17c5ac8f8f17af7bbc476f7707693
Arllain <arllain@gmail.com>
2024-07-14 07:05:23 -0300

    feat:using process

--- apache-camel-tutorial/src/main/java/com/arllain/camel/apache_camel_tutorial/routes/FileMoverRoute.java
+++ apache-camel-tutorial/src/main/java/com/arllain/camel/apache_camel_tutorial/routes/FileMoverRoute.java
@@ -9,7 +9,10 @@ public class FileMoverRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file:{{spring.diretorio-entrada}}")
-                .id("integracao-arquivo")
+                .routeId("integracao-arquivo")
+                .process(exchange -> {
+                    System.out.println(exchange.getMessage().getBody(String.class));
+                })
                 .log("Processando o arquivo: ${file:name}")
 //                .setHeader(FileConstants.FILE_NAME, simple("${date:now:ddMMyyyy_HHmmss}_${file:name}"))
                 .to("file:{{spring.diretorio-saida}}?fileName=${file:onlyname.noext}-${date:now:ddMMyyyy_HHmmss}.${file:ext}");
